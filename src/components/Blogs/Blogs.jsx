819c9ad227dc5cd78363f93fca4d10ccf82afccd
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-04-01 00:30:40 +0600

    Data is fetched succesfully

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Blogs.css';
 const Blogs = () => {
+   const[blogs, setBlogs] = useState([]);
+   useEffect(() =>{
+    fetch('blogs.json')
+    .then(res => res.json())
+    .then(data => setBlogs(data))
+   },[] )
+
     return (
         <div className='blog-container'>
             <div className='individual-blogs'>
-                <h2>LALALALA</h2>
+                <h2>LALALALA {blogs.length} </h2>
             </div>
             <div className='some-details'>
                 <h2>bala</h2>
