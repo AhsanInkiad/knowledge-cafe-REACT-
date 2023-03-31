b7df1662c97da3e13c98548fd9ec2ce308d3b2d1
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-03-31 23:25:29 +0600

    Header is ready

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2 className='title'>Knowledge Cafe</h2>
+            <div>
+                <img className='pic' src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
