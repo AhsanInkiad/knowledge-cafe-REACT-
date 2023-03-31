e4006c0ae9daccbd059337213b4f766a72b09fd6
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-04-01 00:18:43 +0600

    Our fake json is ready

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Blogs.css';
+const Blogs = () => {
+    return (
+        <div className='blog-container'>
+            <div className='individual-blogs'>
+                <h2>LALALALA</h2>
+            </div>
+            <div className='some-details'>
+                <h2>bala</h2>
+            </div>
+
+        </div>
+    );
+};
+
+export default Blogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <h2 className='title'>Knowledge Cafe</h2>
+            <h2 className='title '>Knowledge Cafe</h2>
             <div>
                 <img className='pic' src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
             </div>
